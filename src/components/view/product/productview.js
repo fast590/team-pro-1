2f1b664eceecb433e8abcdcbf2266d222e5577e7
WangDona <develope0407@gmail.com>
2021-03-29 02:42:35 -0700

    product list modification

--- src/components/view/product/productview.js
+++ src/components/view/product/productview.js
@@ -6,10 +6,10 @@ class ProductView extends Component{
     render(){
             const {product} = this.props;
         return(
-            <div className = "w-1/4 float-left m-8">
-                <img src = {product.images[0].src} className ="w-3/4 m-auto"></img>
-                <h3 className ="text-center">{product.name}</h3>
-                <p className = "text-center">{product.price}</p>
+            <div className = "w-3/5 h-40 flex mx-auto py-8">
+                    <img src = {product.images[0].src} className =""></img>
+                    <h3 className ="text-center">{product.name}</h3>
+                    <p className = "text-center">{product.price}</p>
             </div>
         )
     }
